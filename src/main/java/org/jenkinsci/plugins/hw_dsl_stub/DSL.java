0c4060deac4038147228d9a6ec26254d6b181e7a
JeremyMarshall <jeremystuartmarshall@gmail.com>
2015-01-03 09:29:32 +1100

    initial

---
+++ src/main/java/org/jenkinsci/plugins/hw_dsl_stub/DSL.java
@@ -0,0 +1,27 @@
+package org.jenkinsci.plugins.hw_dsl_stub;
+
+import org.jenkinsci.plugins.jobdsl.stub.annotations.dsl.Method;
+import org.jenkinsci.plugins.jobdsl.stub.annotations.dsl.Parameter;
+import org.jenkinsci.plugins.jobdsl.stub.annotations.dsl.Step;
+import hudson.Extension;
+/**
+ * Created by jeremymarshall on 1/01/2015.
+ */
+@Extension
+public class DSL extends org.jenkinsci.plugins.jobdsl.stub.annotations.dsl.Step{
+    @Override
+    public String getName(){
+        return "helloWorld";
+    }
+
+    @Override
+    public String getDescription(){
+        return "Add a Hello World build step";
+    }
+
+    @Method(name="helloWorld", description="Add a helloWorld step")
+    public Object helloWorld(@Parameter(name="name", description="The name to use in the hello world step") String name) {
+        return new HelloWorldBuilder(name);
+    }
+
+}
